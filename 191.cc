e099d91351e38a1f1a8d59bdbf3d29d1af2a7014
Sudheer Dantuluri <dsudheer@gmail.com>
2017-08-31 06:23:41 -0700

    Number of 1 bits

---
+++ 191.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+		{
+    	int count = 0;
+			while (n) {
+				++count;
+				n &= n-1;
+			}
+			return count;   
+    }
+};
+
+int main()
+{
+	return 0;
+}
